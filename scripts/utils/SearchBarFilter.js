02156297215ff0727d579f0fda7ea726db0405e2
basthub <gallo.bastien@outlook.com>
2022-08-10 23:36:51 +0200

    refactoring + esLint

--- scripts/utils/SearchBarFilter.js
+++ scripts/utils/SearchBarFilter.js
@@ -1,28 +1,14 @@
 import { searchBar } from '../pages/Index.js'
 
 function filteredBySearchBar (recipes) {
-  const filteredArray = []
+  const filteredArray = recipes.filter((recipe) => {
+    return (
+      recipe.name.toLowerCase().includes(searchBar.value.toLowerCase()) ||
+      recipe.description.toLowerCase().includes(searchBar.value.toLowerCase()) ||
+      recipe.ingredients.includes((ingredient) => ingredient.ingredient.toLowerCase().includes(searchBar.value.toLowerCase()))
+    )
+  })
 
-  for (let rcp = recipes.length; rcp >= 0; rcp--) {
-    let containsName = false
-    let containsDescription = false
-    let containsIngredients = false
-
-    if (recipes[rcp]?.name.toLowerCase().includes(searchBar.value.toLowerCase())) {
-      containsName = true
-    }
-    for (let igd = 0; igd < recipes[rcp]?.ingredients[igd]?.ingredient.length; igd++) {
-      if (recipes[rcp].ingredients[igd]?.ingredient?.toLowerCase().includes(searchBar.value.toLowerCase())) {
-        containsIngredients = true
-      }
-    }
-    if (recipes[rcp]?.description.toLowerCase().includes(searchBar.value.toLowerCase())) {
-      containsDescription = true
-    }
-    if (containsName || containsIngredients || containsDescription) {
-      filteredArray.push(recipes[rcp])
-    }
-  }
   return filteredArray
 }
 export default filteredBySearchBar
